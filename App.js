afec3d9f657a4b223e1a5a729276ece9e0ac3f4d
HendraLatumeten <latumetenhendra@gmail.com>
2023-03-02 11:25:04 +0700

    test

--- App.js
+++ App.js
@@ -1,52 +1,96 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
+import * as Progress from 'react-native-progress';
 import {Provider} from 'react-redux';
 import AppNavigator from './src/navigations/AppNavigator';
 import {PersistGate} from 'redux-persist/integration/react';
 import {persistor, store} from './src/states/store';
-import {Alert, Text} from 'react-native';
+import {ActivityIndicator, Alert, Text, View} from 'react-native';
 import {color} from './src/styles';
 import CodePush from 'react-native-code-push';
 import {Loading} from './src/components/atoms';
 
-let CodePushOptions = {
-  checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
-  mandatoryInstallMode: CodePush.InstallMode.IMMEDIATE,
-  updateDialog: {
-    appendReleaseDescription: true,
-    title: 'a new update is available!',
-  },
-  onDownloadProgress: progress => {
-    setDownloadProgress(progress.receivedBytes / progress.totalBytes);
-
-    if (progress.receivedBytes === progress.totalBytes) {
-      // Tampilkan notifikasi ketika pengunduhan selesai
-      Alert.alert(
-        'Update berhasil diunduh',
-        'Aplikasi akan memperbarui dirinya saat dibuka kembali',
-      );
-    }
-  },
-};
-if (CodePushOptions.checkFrequency == 1) {
-  Alert.alert('test');
-  console.log('test', CodePushOptions.onDownloadProgress.length);
-}
-console.log('oke', CodePush.CheckFrequency);
-
 const App = () => {
-  Text.defaultProps = Text.defaultProps || {};
-  Text.defaultProps.style = {
-    fontSize: 14,
-    fontFamily: 'Poppins-Regular',
-    color: color.p800,
-  };
+  const [progressBar, setProgress] = useState(0);
+  const [showPopup, setShowPopup] = useState(false);
+
+  useEffect(() => {
+    const downloadCodePush = async () => {
+      try {
+        const update = await CodePush.checkForUpdate();
+        console.log('update', update);
+        console.log('CodePush', CodePush);
+        if (!update) {
+          console.log('The app is up to date.');
+        } else {
+          setShowPopup(true);
+          const syncStatus = CodePush.sync(
+            {updateDialog: true, installMode: CodePush.InstallMode.IMMEDIATE},
+            status => {
+              console.log(`CodePush sync status: ${JSON.stringify(status)}`);
+              if (status === CodePush.SyncStatus.DOWNLOADING_PACKAGE) {
+                console.log(
+                  `Download progress: ${syncStatus.downloadProgress}`,
+                );
+                setProgress(Math.floor(syncStatus.downloadProgress * 100));
+              }
+            },
+          );
+          await CodePush.sync(
+            {
+              checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
+              mandatoryInstallMode: CodePush.InstallMode.IMMEDIATE,
+              updateDialog: {
+                appendReleaseDescription: true,
+                title: 'A new update is available!',
+              },
+              installMode: CodePush.InstallMode.IMMEDIATE,
+            },
+            syncStatus,
+          );
+        }
+      } catch (err) {
+        console.log(`An error occurred while checking for updates - ${err}`);
+      } finally {
+        setShowPopup(false);
+      }
+    };
+
+    downloadCodePush();
+  }, []);
+
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <AppNavigator />
+        <View style={{flex: 1}}>
+          <AppNavigator />
+          {showPopup && (
+            <View
+              style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                bottom: 0,
+                right: 0,
+                alignItems: 'center',
+                justifyContent: 'center',
+                backgroundColor: 'rgba(0, 0, 0, 0.5)',
+              }}>
+              <View
+                style={{
+                  backgroundColor: '#fff',
+                  padding: 20,
+                  borderRadius: 10,
+                  alignItems: 'center',
+                }}>
+                <Text>Downloading update: {progressBar}%</Text>
+                <Progress.Circle progress={progressBar / 100} size={50} />
+              </View>
+            </View>
+          )}
+        </View>
       </PersistGate>
     </Provider>
   );
 };
 
-export default CodePush(CodePushOptions)(App);
+export default CodePush(App);
